1e507c4073a2025b29d25de868094876798ce60f
akonst17 <akonst17@gmail.com>
2022-04-14 16:15:41 +0300

    upgrade; build 3

--- SIE/Classes/Misc/Appearance.swift
+++ SIE/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 25, green: 63, blue: 189)
     static let progress2Color = UIColor(integralRed: 230, green: 125, blue: 56)
     static let progress3Color = UIColor(integralRed: 241, green: 180, blue: 22)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
--- SIE/Classes/Service/Stats/Share/StatsShareManager.swift
+++ SIE/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.sie.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.sie.widget")
     }
     
     @discardableResult
