75df3d7fc851dee84787c6fb404e48775b525f0f
akonst17 <akonst17@gmail.com>
2022-09-21 11:54:25 +0300

    интеграция sdk 2.0; новые урлы; build 7

--- SIE/Classes/Misc/GlobalDefinitions.swift
+++ SIE/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "64P1aommP0QGfGdL"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "6kcL7qhQwgPcKOdI"
 
     static let applicationTag = "EXAM_SIE"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "6kcL7qhQwgPcKOdI"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "4a4fH0Vnk3NVKX4r"
 
     static let appStoreUrl = "https://apps.apple.com/app/sie-exam/id1584509838"
     static let appleAppID = "1584509838"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
